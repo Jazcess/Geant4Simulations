21d7df73f2cd693604439c3e3e03911014d65fee
Jazmin Stewart <35260699+Jazcess@users.noreply.github.com>
2023-01-30 16:12:45 +0000

    Deleting Cmake files

--- scintillationSim/CMakeFiles/ScintillationSim.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ScintillationSim"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ScintillationSim.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
