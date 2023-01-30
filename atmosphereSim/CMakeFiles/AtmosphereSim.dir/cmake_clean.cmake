f8363bdc107ddd4c4d2970cb8cb5a9dc621a4990
Jazmin Stewart <35260699+Jazcess@users.noreply.github.com>
2023-01-30 15:18:39 +0000

    Cherenkov and atmopshere
    Cherenkov and particle shower simulations uploaded

---
+++ atmosphereSim/CMakeFiles/AtmosphereSim.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AtmosphereSim"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/AtmosphereSim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ atmosphereSim/CMakeFiles/atmosphereSim.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/j/jss55/Software/geant4/atmosphereSim/action.cc" "CMakeFiles/atmosphereSim.dir/action.cc.o" "gcc" "CMakeFiles/atmosphereSim.dir/action.cc.o.d"
+  "/home/j/jss55/Software/geant4/atmosphereSim/atmosphereSim.cc" "CMakeFiles/atmosphereSim.dir/atmosphereSim.cc.o" "gcc" "CMakeFiles/atmosphereSim.dir/atmosphereSim.cc.o.d"
+  "/home/j/jss55/Software/geant4/atmosphereSim/construction.cc" "CMakeFiles/atmosphereSim.dir/construction.cc.o" "gcc" "CMakeFiles/atmosphereSim.dir/construction.cc.o.d"
+  "/home/j/jss55/Software/geant4/atmosphereSim/detector.cc" "CMakeFiles/atmosphereSim.dir/detector.cc.o" "gcc" "CMakeFiles/atmosphereSim.dir/detector.cc.o.d"
+  "/home/j/jss55/Software/geant4/atmosphereSim/generator.cc" "CMakeFiles/atmosphereSim.dir/generator.cc.o" "gcc" "CMakeFiles/atmosphereSim.dir/generator.cc.o.d"
+  "/home/j/jss55/Software/geant4/atmosphereSim/physics.cc" "CMakeFiles/atmosphereSim.dir/physics.cc.o" "gcc" "CMakeFiles/atmosphereSim.dir/physics.cc.o.d"
+  "/home/j/jss55/Software/geant4/atmosphereSim/run.cc" "CMakeFiles/atmosphereSim.dir/run.cc.o" "gcc" "CMakeFiles/atmosphereSim.dir/run.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
