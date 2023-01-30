e1945e0684907f86cd1f3ca770d5fed64a7fa831
Jazmin Stewart <35260699+Jazcess@users.noreply.github.com>
2023-01-30 15:23:33 +0000

    atmosphere sim didnt uplaod correctly

--- atmosphereSim/CMakeFiles/AtmosphereSim.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/AtmosphereSim"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/AtmosphereSim.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- atmosphereSim/CMakeFiles/atmosphereSim.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for atmosphereSim.
-# This may be replaced when dependencies are built.
